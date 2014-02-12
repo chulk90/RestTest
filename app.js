36077be1f2233a55faea819d439726e869ffead1
jacksongeller <jacksongeller@gmail.com>
2014-02-11 23:07:27 -0500

    need to add update and remove

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/restfulAPI');
+var db = mongoose.connection;
+
+var bookSchema = mongoose.Schema({
+	name: String,
+	author: String,
+	copyRight: Number
+});
+
+var Book = mongoose.model('Book', bookSchema);
 
 
 var app = express();
@@ -25,12 +35,11 @@ app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.methodOverride());
 app.use(express.bodyParser());
+app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 
 // development only
@@ -38,9 +47,63 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.get('/', function(req, res){
+	res.render('index');
+})
 
 
 // Create
+app.post('/books', function(req, res){
+	console.log(req);
+	Book.create({
+		name: req.body.name,
+		author: req.body.author,
+		copyRight: req.body.copy
+	}, function(err, docs){
+		if (err){
+			res.send(err);
+		} else{
+			res.send(docs);
+		}
+	})
+})
+
+
+// Edit
+
+
+
+// remove
+
+
+
+
+// View one
+
+app.get('/books/:name', function(req, res){
+	Book.find({name: req.params.name}, function(err, docs){
+		if (err){
+			res.send(err)
+		} else{
+			res.send(docs)
+		}
+	})
+});
+
+
+
+
+// View all
+app.get('/all', function(req, res){
+	Book.find({}, function(err, docs){
+		res.send(docs);
+	})
+})
+
+
+
+// Home page
+
 
 
 
